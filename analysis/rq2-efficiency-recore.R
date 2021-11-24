c7ebfd8a11ab2c059833c69e4efb5a7b991fd462
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2021-11-24 22:09:17 +0100

    Add extra figures and tables

--- analysis/rq2-efficiency-recore.R
+++ analysis/rq2-efficiency-recore.R
@@ -44,14 +44,6 @@ for (ca1 in configurations){
   
 }
 
-
-# 
-# delta <- timeEffectSize%>% 
-#   mutate(delta = (avg.alg2 - avg.alg1)/avg.alg2) %>%
-#   group_by(Winner) %>%
-#   summarise(avg_delta = mean(delta))
-#   
-
 efficiency_abstract$count <- as.numeric(efficiency_abstract$count)
 p <- ggplot(data=efficiency_abstract, aes(x=Winner, y=count, fill=VD_magnitude)) +
   geom_bar(stat="identity", position=position_dodge()) +
@@ -63,6 +55,89 @@ p <- ggplot(data=efficiency_abstract, aes(x=Winner, y=count, fill=VD_magnitude))
   ylab("# of crashes") + xlab("Configurations") +
   theme(legend.position = "top", legend.text=element_text(size=14), axis.text.x=element_text(size=14))
 ggsave("figures/significant-efficiency-recore.pdf", width = 5.5, height = 4)
+
+
+##### Appendix #####
+
+
+efficiency_abstract <- data.frame('Winner'=NA, 'count' = NA, 'VD_magnitude' =NA)
+efficiency_abstract <- efficiency_abstract[0,]
+
+
+configurations <- c("RecoreSTDistance","RecoreSTDistance+old-BBC")
+
+
+for (ca1 in configurations){
+  for(ca2 in configurations){
+    if (ca1 == ca2){
+      next
+    }
+    for (mag in CONSIDERABLE_VD_MAGNITUDES){
+      row = c(ca1)
+      tempdf <- timeEffectSize %>%
+        filter(cat.alg1 == ca1 & cat.alg2 == ca2 & VD.magnitude == mag & VD.estimate.category=="< 0.5")
+      row <- c(row,nrow(tempdf),mag)
+      efficiency_abstract[nrow(efficiency_abstract) + 1,] = row
+    }
+  }
+  
+}
+
+efficiency_abstract$count <- as.numeric(efficiency_abstract$count)
+p <- ggplot(data=efficiency_abstract, aes(x=Winner, y=count, fill=VD_magnitude)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_text(aes(label=count), vjust=-1.0, color="black",
+            position = position_dodge(0.9), size=5.5)+
+  theme(text = element_text(size=14),axis.text.x = element_text(size=11)) +
+  scale_y_continuous(limits=c(0, 59), expand = c(0, 0), breaks = NULL) +
+  scale_fill_grey(start = 0, end = .9) +
+  ylab("# of crashes") + xlab("Configurations") +
+  theme(legend.position = "top", legend.text=element_text(size=14), axis.text.x=element_text(size=14))
+ggsave("figures/significant-efficiency-recore-oldvsnone.pdf", width = 5.5, height = 4)
+
+
+
+
+
+efficiency_abstract <- data.frame('Winner'=NA, 'count' = NA, 'VD_magnitude' =NA)
+efficiency_abstract <- efficiency_abstract[0,]
+
+
+configurations <- c("RecoreSTDistance+old-BBC","RecoreSTDistance+BBC")
+
+
+for (ca1 in configurations){
+  for(ca2 in configurations){
+    if (ca1 == ca2){
+      next
+    }
+    for (mag in CONSIDERABLE_VD_MAGNITUDES){
+      row = c(ca1)
+      tempdf <- timeEffectSize %>%
+        filter(cat.alg1 == ca1 & cat.alg2 == ca2 & VD.magnitude == mag & VD.estimate.category=="< 0.5")
+      row <- c(row,nrow(tempdf),mag)
+      efficiency_abstract[nrow(efficiency_abstract) + 1,] = row
+    }
+  }
+  
+}
+
+efficiency_abstract$count <- as.numeric(efficiency_abstract$count)
+p <- ggplot(data=efficiency_abstract, aes(x=Winner, y=count, fill=VD_magnitude)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_text(aes(label=count), vjust=-1.0, color="black",
+            position = position_dodge(0.9), size=5.5)+
+  theme(text = element_text(size=14),axis.text.x = element_text(size=11)) +
+  scale_y_continuous(limits=c(0, 59), expand = c(0, 0), breaks = NULL) +
+  scale_fill_grey(start = 0, end = .9) +
+  ylab("# of crashes") + xlab("Configurations") +
+  theme(legend.position = "top", legend.text=element_text(size=14), axis.text.x=element_text(size=14))
+ggsave("figures/significant-efficiency-recore-oldvsbbc.pdf", width = 6.5, height = 4)
+
+#################### 
+
+
+
 
 mutateTimeEffectSize <- timeEffectSize %>%
   # filter(cat.alg1 == "RecoreSTDistance+BBC" & cat.alg2 == "RecoreSTDistance") %>%
